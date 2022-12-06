e4618f37cf92656bccdbb6857d363f640e55898a
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-07 01:43:21 +0300

    Install script updated

--- setup/tools/dotnet_ef_install.cmd
+++ setup/tools/dotnet_ef_install.cmd
@@ -5,6 +5,8 @@
 TITLE Installing Entity Framework CLI
 
 :: Устанавливает инструменты Entity Framework
-dotnet tool install --global dotnet-ef
+:: https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-tool-install
+:: https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-tool-uninstall
+dotnet tool install --global --framework net6.0 dotnet-ef
 
 PAUSE
