c4bd7592b979dd6503ddf24494e56dd1c1451d49
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-14 20:54:16 +0300

    Updated

--- __remove_BIN_and_OBJ_folders.bat
+++ __remove_BIN_and_OBJ_folders.bat
@@ -7,7 +7,7 @@ TITLE "Deleting all BIN and OBJ folders"
 @ECHO Deleting all BIN and OBJ folders...
 
 :: В цикле удаляет папки
-FOR /D /R . %%D IN (bin,obj) DO @if EXIST "%%D" RD /S/Q "%%D"
+FOR /D /R . %%D IN (bin,obj) DO @IF EXIST "%%D" RD /S/Q "%%D"
 
 @ECHO BIN and OBJ folders successfully deleted :) Close the window.
 
